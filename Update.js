806dd7a04b649e5e70dcd28cd1cfe29968b1c6a9
jellluana <116643108+jellluana@users.noreply.github.com>
2023-01-19 12:46:32 +0800

    Create Update.js

---
+++ Update.js
@@ -0,0 +1,17 @@
+
+const Connection = require('../database/Connection')
+
+module.exports = async (fields) => {
+  try {
+    const query = `SELECT ` +
+                    `${fields} ` +
+                  `FROM ` +
+                    `accounts`
+
+    const results = await Connection(query)
+
+    return results
+  } catch (err) {
+    return []
+  }
+}
